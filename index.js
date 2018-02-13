878d840424ff3d199ef3841b9ea3f69cc71e38f7
s.auguste <stephane.auguste@calvados.fr>
2018-02-13 14:19:51 +0100

    change index.js

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passport');
+require('./models/User');
+
+mongoose.connect(keys.mongoURI);
+
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
-});
+// app.use(
+//     cookieSession({
+//         maxAge: 30 * 24 * 60 * 60 * 1000,
+//         keys: [keys.cookieKey]
+//     })
+// );
+// app.use(passport.initialize());
+// app.use(passport.session());
 
+require('./routes/authRoutes')(app);
+//console.developers.google.com
+
+// Lancement du serveur sur le port 5000
 const PORT = process.env.PORT || 5000;
-app.listen(5000);
+app.listen(PORT);
