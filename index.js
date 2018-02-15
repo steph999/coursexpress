d2980361593c4a6c643699e1bf4aba31b98d50fc
s.auguste <stephane.auguste@calvados.fr>
2018-02-15 12:15:16 +0100

    change index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./services/passport');
 require('./models/User');
 
 mongoose.connect(keys.mongoURI);
-
 
 const app = express();
 
