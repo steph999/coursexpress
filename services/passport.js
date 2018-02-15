0f4199c2a965e95aae1c2e3bebbfb4f40c9f8131
s.auguste <stephane.auguste@calvados.fr>
2018-02-15 16:39:30 +0100

    change index.js

--- services/passport.js
+++ services/passport.js
@@ -24,14 +24,14 @@ passport.use(
             proxy: true
         },
         (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id }).then(existingUser);
-            if (existingUser) {
-                done(null, existingUser);
-            } else {
-                new User({ googleId: profile.id})
-                    .save()
-                    .then(user => done(null, user));
-            }
+            //User.findOne({ googleId: profile.id }).then(existingUser);
+            //if (existingUser) {
+            //    done(null, existingUser);
+            //} else {
+            //    new User({ googleId: profile.id})
+            //        .save()
+            //        .then(user => done(null, user));
+            //}
             done(null,{
                 ok:'ok'
             });
