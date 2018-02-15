f07c4e4e4867291d5948b21d80d060d3f731be16
s.auguste <stephane.auguste@calvados.fr>
2018-02-15 14:03:19 +0100

    change index.js

--- services/passport.js
+++ services/passport.js
@@ -7,7 +7,7 @@ passport.use(
         {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback',
+            callbackURL: 'https://auguste-cours-node.herokuapp.com/auth/google/callback',
             proxy: true
         }
     )
