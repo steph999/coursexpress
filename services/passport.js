cbd64b8f47f00597ee5742be02c2866449ef8725
s.auguste <stephane.auguste@calvados.fr>
2018-02-15 16:24:49 +0100

    change index.js

--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,18 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then (user => {
+        done(null, user);
+    });
+});
+
 passport.use(
     new GoogleStrategy(
         {
@@ -11,12 +23,14 @@ passport.use(
             proxy: true
         },
         (accessToken, refreshToken, profile, done) => {
-            //User.findOne({ googleId: profile.id }).then(existingUser);
-            //if (existingUser) {
-            //    done(null, existingUser);
-            //} else {
-//
-            //}
+            User.findOne({ googleId: profile.id }).then(existingUser);
+            if (existingUser) {
+                done(null, existingUser);
+            } else {
+                new User({ googleId: profile.id})
+                    .save()
+                    .then(user => done(null, user));
+            }
             done(null,{
                 ok:'ok'
             });
