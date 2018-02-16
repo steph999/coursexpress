9da6f29da9e6684cf0be0611eb66e82d2f41d703
s.auguste <stephane.auguste@calvados.fr>
2018-02-16 10:40:07 +0100

    change index.js

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports ={
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    googleRedirectURI: 'https://auguste-cours-node.herokuapp.com'
+    // googleRedirectURI: 'https://auguste-cours-node.herokuapp.com',
     cookieKey: process.env.COOKIE_KEY
 };
