a890d8216ff4feeb4fee9d36efcc8b95d9699e48
s.auguste <stephane.auguste@calvados.fr>
2018-02-16 14:36:03 +0100

    change index.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    googleId: String,
-    nom: String
-    // nom: {
-    //     familyName: String,
-    //     givenName: String
-    // }
-}, {collection: 'users'});
+    googleId: String
+}, { collection : 'users'});
 
 module.exports = mongoose.model('User', userSchema, 'users');
